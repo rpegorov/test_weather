ca87b71163dc8fbf80df6bc7fa6887b40d09ff6e
rpegorov <rostislav.egorov@gmail.com>
2021-02-06 23:16:38 +0300

    Refactoring

--- app/src/main/java/rpegorov/weather/RecordActivity.java
+++ app/src/main/java/rpegorov/weather/RecordActivity.java
@@ -2,38 +2,37 @@ package rpegorov.weather;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.media.*;
+import android.media.AudioAttributes;
+import android.media.AudioRecord;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.media.audiofx.NoiseSuppressor;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import java.io.IOException;
-import java.util.Random;
+import java.util.UUID;
 
-import static android.Manifest.permission.RECORD_AUDIO;
-import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
-import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.*;
 import static rpegorov.weather.R.layout.activity_record;
 
 /**
  * Класс записи и воспроизведения записанного звука
  */
 public class RecordActivity extends AppCompatActivity {
-    Button buttonStart, buttonStop, buttonPlayLastRecordAudio,
-            buttonStopPlayingRecording;
-    String AudioSavePathInDevice = null;
-    MediaRecorder mediaRecorder;
-    AudioRecord audioRecord;
-    Random random;
-    String RandomAudioFileName = "QWERTY";
-    public static final int RequestPermissionCode = 1;
-    MediaPlayer mediaPlayer;
+    private Button buttonStart, buttonStop, buttonPlayLastRecordAudio, buttonStopPlayingRecording;
+    private String AudioSavePathInDevice = null;
+    private MediaRecorder mediaRecorder;
+    public AudioRecord audioRecord;
+    private static final int RequestPermissionCode = 1;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,46 +47,42 @@ public class RecordActivity extends AppCompatActivity {
         buttonPlayLastRecordAudio.setEnabled(false);
         buttonStopPlayingRecording.setEnabled(false);
 
-        random = new Random();
         String[] permissions = {Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE};
         ActivityCompat.requestPermissions(this, permissions, RequestPermissionCode);
 
-/** Метод инициазирует запись звука по клику кнопки
- * @AudioSavePathInDevice - указывает путь созранения и методом
- * @CreateRandomAudioFileName генерирует случайное имя файла
- */
-        buttonStart.setOnClickListener(new View.OnClickListener() {
+        buttonStart.setOnClickListener(createStartRecordClickListener());
+        buttonStop.setOnClickListener(createStopClickListener());
+        buttonPlayLastRecordAudio.setOnClickListener(createPlayLastRecordAudioListener());
+        buttonStopPlayingRecording.setOnClickListener(createStopPlayingRecording());
+    }
+
+    private View.OnClickListener createStartRecordClickListener() {
+        return new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 if (checkPermission()) {
-
                     AudioSavePathInDevice =
                             Environment.getExternalStorageDirectory().getAbsolutePath() + "/" +
-                                    CreateRandomAudioFileName(5) + "AudioRec.3gp";
-
-                    MediaRecorderReady();
-
+                                    UUID.randomUUID().toString() + "AudioRec.3gp";
+                    prepareMediaRecorder();
                     try {
                         mediaRecorder.prepare();
                         mediaRecorder.start();
+                        buttonStart.setEnabled(false);
+                        buttonStop.setEnabled(true);
+                        Toast.makeText(RecordActivity.this, "Recording started", Toast.LENGTH_LONG).show();
                     } catch (IllegalStateException | IOException e) {
-                        e.printStackTrace();
+                        Toast.makeText(RecordActivity.this, "Error on Start records", Toast.LENGTH_LONG).show();
                     }
-
-                    buttonStart.setEnabled(false);
-                    buttonStop.setEnabled(true);
-
-                    Toast.makeText(RecordActivity.this, "Recording started",
-                            Toast.LENGTH_LONG).show();
                 } else {
                     requestPermission();
                 }
-
             }
-        });
-/** Метод остановки записи звука по клику кнопки*/
-        buttonStop.setOnClickListener(new View.OnClickListener() {
+        };
+    }
+
+    private View.OnClickListener createStopClickListener() {
+        return new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mediaRecorder.stop();
@@ -95,84 +90,76 @@ public class RecordActivity extends AppCompatActivity {
                 buttonPlayLastRecordAudio.setEnabled(true);
                 buttonStart.setEnabled(true);
                 buttonStopPlayingRecording.setEnabled(false);
-
-                Toast.makeText(RecordActivity.this, "Recording Completed",
-                        Toast.LENGTH_LONG).show();
+                Toast.makeText(RecordActivity.this, "Recording Completed", Toast.LENGTH_LONG).show();
             }
-        });
-/** Метод проигрывания последнего файла
- * @setDataSource указывает на файл для проигрывания
- * */
-        buttonPlayLastRecordAudio.setOnClickListener(new View.OnClickListener() {
+        };
+    }
+
+    private View.OnClickListener createPlayLastRecordAudioListener() {
+        return new View.OnClickListener() {
             @Override
             public void onClick(View view) throws IllegalArgumentException,
                     SecurityException, IllegalStateException {
-
                 buttonStop.setEnabled(false);
                 buttonStart.setEnabled(false);
                 buttonStopPlayingRecording.setEnabled(true);
-
                 mediaPlayer = new MediaPlayer();
-                MediaPlayerReady();
-
+                prepareMediaPlayer();
                 try {
                     mediaPlayer.setDataSource(AudioSavePathInDevice);
                     mediaPlayer.prepare();
-
+                    mediaPlayer.start();
+                    Toast.makeText(RecordActivity.this, "Recording Playing", Toast.LENGTH_LONG).show();
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    Toast.makeText(RecordActivity.this, "Error on Play last Audio", Toast.LENGTH_LONG).show();
                 }
-                mediaPlayer.start();
-
-                Toast.makeText(RecordActivity.this, "Recording Playing",
-                        Toast.LENGTH_LONG).show();
             }
-        });
-/** Реализация остановки проигрывания файла по клику*/
-        buttonStopPlayingRecording.setOnClickListener(new View.OnClickListener() {
+        };
+    }
+
+    private View.OnClickListener createStopPlayingRecording() {
+        return new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 buttonStop.setEnabled(false);
                 buttonStart.setEnabled(true);
                 buttonStopPlayingRecording.setEnabled(false);
                 buttonPlayLastRecordAudio.setEnabled(true);
-
                 if (mediaPlayer != null) {
                     mediaPlayer.stop();
                     mediaPlayer.release();
-                    MediaRecorderReady();
+                    prepareMediaRecorder();
                 }
             }
-        });
+        };
     }
 
-    /**Реализация класса  MediaRecorder
-     * @AudioSource указывает на источник записи
-     * @OutputFormat устанавливает исходящий формат
-     * @AudioEncoder выбор кодека кодирования
-     * @setOutputFile расположение исходящего файла
-     * @NoiseSuppressor.isAvailable() проверка на наличие поддержки шумоподавления
-     * @getAudioSessionId выбор сессии шумоподавления
+    /**
+     * Реализация класса  MediaRecorder
+     * AudioSource указывает на источник записи
+     * OutputFormat устанавливает исходящий формат
+     * AudioEncoder выбор кодека кодирования
+     * setOutputFile расположение исходящего файла
+     * NoiseSuppressor.isAvailable() проверка на наличие поддержки шумоподавления
+     * getAudioSessionId выбор сессии шумоподавления
      */
-    public void MediaRecorderReady() {
+    private void prepareMediaRecorder() {
         mediaRecorder = new MediaRecorder();
-        if  (NoiseSuppressor.isAvailable()) {
+        if (NoiseSuppressor.isAvailable()) {
             NoiseSuppressor.create(audioRecord.getAudioSessionId());
         }
         mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);  // THREE_GPP
-        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);  // AMR_NB
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
         mediaRecorder.setOutputFile(AudioSavePathInDevice);
     }
 
     /**
      * Реализация класса MediaPlayer
-     *
-     * @AudioAttributes установка контекста контента
-     * @AudioAttributes установка типа контента
-     * Может быть этот метод и не нужен, надо проверять...
+     * AudioAttributes установка контекста контента
+     * AudioAttributes установка типа контента
      */
-    public void MediaPlayerReady() {
+    private void prepareMediaPlayer() {
         mediaPlayer = new MediaPlayer();
         mediaPlayer.setAudioAttributes(
                 new AudioAttributes.Builder()
@@ -180,21 +167,6 @@ public class RecordActivity extends AppCompatActivity {
                         .setUsage(AudioAttributes.USAGE_MEDIA)
                         .build()
         );
-    }
-
-    /**
-     * Метод создания рандомного имени файла
-     */
-    public String CreateRandomAudioFileName(int string) {
-        StringBuilder stringBuilder = new StringBuilder(string);
-        int i = 0;
-        while (i < string) {
-            stringBuilder.append(RandomAudioFileName.
-                    charAt(random.nextInt(RandomAudioFileName.length())));
-
-            i++;
-        }
-        return stringBuilder.toString();
     }
 
     /**
@@ -208,38 +180,21 @@ public class RecordActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case RequestPermissionCode:
-                if (grantResults.length > 0) {
-                    boolean StoragePermission = grantResults[0] ==
-                            PackageManager.PERMISSION_GRANTED;
-                    boolean RecordPermission = grantResults[1] ==
-                            PackageManager.PERMISSION_GRANTED;
-
-                    if (StoragePermission && RecordPermission) {
-                        Toast.makeText(RecordActivity.this, "Permission Granted",
-                                Toast.LENGTH_LONG).show();
-                    } else {
-                        Toast.makeText(RecordActivity.this, "Permission Denied",
-                                Toast.LENGTH_LONG).show();
-                    }
-                }
-                break;
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == RequestPermissionCode) {
+            if (grantResults.length > 0) {
+                boolean storagePermission = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                boolean recordPermission = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                String msg = storagePermission && recordPermission ? "Permission Granted" : "Permission Denied";
+                Toast.makeText(RecordActivity.this, msg, Toast.LENGTH_LONG).show();
+            }
         }
     }
 
-    /**
-     * Метод проверки разрешений
-     */
-    public boolean checkPermission() {
-        int result = ContextCompat.checkSelfPermission(getApplicationContext(),
-                WRITE_EXTERNAL_STORAGE);
-        int result2 = ContextCompat.checkSelfPermission(getApplicationContext(),
-                READ_EXTERNAL_STORAGE);
-        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(),
-                RECORD_AUDIO);
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+        int result2 = ContextCompat.checkSelfPermission(getApplicationContext(), READ_EXTERNAL_STORAGE);
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), RECORD_AUDIO);
         return result == PackageManager.PERMISSION_GRANTED &&
                 result1 == PackageManager.PERMISSION_GRANTED &&
                 result2 == PackageManager.PERMISSION_GRANTED;
--- app/src/main/java/rpegorov/weather/WeatherActivity.java
+++ app/src/main/java/rpegorov/weather/WeatherActivity.java
@@ -13,15 +13,20 @@ import org.json.JSONObject;
 
 import static rpegorov.weather.R.layout.activity_weather;
 
-/** Клас для получения данных о погоде на текущий момент */
+/**
+ * Клас для получения данных о погоде на текущий момент
+ */
 
 public class WeatherActivity extends AppCompatActivity {
 
     TextView maxLayout, minLayout,
             viewMin, viewMax, citySearch;
-    /** Вызывается при входе в "полноценное состояние" инициализирует активити (activity_weather)
+
+    /**
+     * Вызывается при входе в "полноценное состояние" инициализирует активити (activity_weather)
      * AndroidNetworking - включает запросы на сервер и извлечение возвращенных данных
-     * findViewById - позволяет получить ссылку на View и инициализировать объект */
+     * findViewById - позволяет получить ссылку на View и инициализировать объект
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,15 +39,19 @@ public class WeatherActivity extends AppCompatActivity {
         citySearch = findViewById(R.id.search_bar);
     }
 
-    /** В данном методе инициализируем метод getWeatherData и передаем в него параметры (название города)
-     * введеные пользователем в search_bar*/
+    /**
+     * В данном методе инициализируем метод getWeatherData и передаем в него параметры (название города)
+     * введеные пользователем в search_bar
+     */
     public void updateDataWeather(View view) {
         getWeatherData(citySearch.getText().toString());
     }
 
-    /** С помощью AndroidNetworking делаем запрос на url и передаем параметры (addQueryParameter)
-     * методом getAsJSONObject получаем JSON который преобразуем объект */
-    public void getWeatherData(String cityName) {
+    /**
+     * С помощью AndroidNetworking делаем запрос на url и передаем параметры (addQueryParameter)
+     * методом getAsJSONObject получаем JSON который преобразуем объект
+     */
+    private void getWeatherData(String cityName) {
         AndroidNetworking.get(getResources().getString(R.string.api_open_weather_map_url))
                 .addQueryParameter(getResources().getString(R.string.city_key), cityName)
                 .addQueryParameter(getResources().getString(R.string.metric), getResources().getString(R.string.metric_value))
@@ -50,24 +59,23 @@ public class WeatherActivity extends AppCompatActivity {
                 .build()
                 .getAsJSONObject(new JSONObjectRequestListener() {
                     /** onResponse используется для чтения данных из объекта JSON JSONObject response
-                    * методом setText в объект (поле) viewMax записываем данные из метода getJSONObject
-                    * getJSONObject - извлекает в объекте JSON параметр "main" из него извлекаем парамтер "temp_max" */
+                     * методом setText в объект (поле) viewMax записываем данные из метода getJSONObject
+                     * getJSONObject - извлекает в объекте JSON параметр "main" из него извлекаем парамтер "temp_max" */
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
                             viewMax.setText(String.valueOf(response.getJSONObject("main").getDouble("temp_max")));
                             viewMin.setText(String.valueOf(response.getJSONObject("main").getDouble("temp_min")));
                         } catch (JSONException e) {
-                            e.printStackTrace();
+                            Toast.makeText(WeatherActivity.this, getResources().getString(R.string.not_found), Toast.LENGTH_LONG).show();
                         }
                     }
-/** В случае какой либо ошибки вывыести сообщение из строки */
+
+                    /** В случае какой либо ошибки вывыести сообщение из строки */
                     @Override
                     public void onError(ANError anError) {
-                        Toast.makeText(WeatherActivity.this, R.string.not_found,
-                                Toast.LENGTH_LONG).show();
+                        Toast.makeText(WeatherActivity.this, "Error", Toast.LENGTH_LONG).show();
                     }
                 });
-
     }
 }
